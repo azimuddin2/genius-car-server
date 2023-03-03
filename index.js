08bd0846f9f9ed71e20e15db59d6c03e8e056875
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-03-03 21:22:02 +0600

    added is service find operation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -8,6 +9,28 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lxrpuhw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db("geniusCar").collection("service");
+
+        // Service Operation
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Genius car server running');
