82bece01b579b38db02bc8c6f1dfd234e2ec7257
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-04-01 23:52:26 +0600

    added a payment oparation

--- index.js
+++ index.js
@@ -61,51 +61,95 @@ async function run() {
 
 
 
-        //Order Operation
+        //Order & Payment Operation
         app.post('/order', async (req, res) => {
             const order = req.body;
+            const { serviceName, servicePrice, customer, email, address, phone, postcode, currency } = order;
+            const transactionId = new ObjectId().toString();
 
             const data = {
-                total_amount: order.servicePrice,
-                currency: order.currency,
-                tran_id: new ObjectId().toString(),
-                success_url: 'http://localhost:3030/success',
-                fail_url: 'http://localhost:3030/fail',
-                cancel_url: 'http://localhost:3030/cancel',
-                ipn_url: 'http://localhost:3030/ipn',
+                total_amount: servicePrice,
+                currency: currency,
+                tran_id: transactionId,
+                success_url: `${process.env.SERVER_URL}/payment/success?transactionId=${transactionId}`,
+                fail_url: `${process.env.SERVER_URL}/payment/fail?transactionId=${transactionId}`,
+                cancel_url: 'http://localhost:5000/payment/cancel',
+                ipn_url: 'http://localhost:5000/payment/ipn',
                 shipping_method: 'Courier',
-                product_name: order.serviceName,
+                product_name: serviceName,
                 product_category: 'Electronic',
                 product_profile: 'general',
-                cus_name: order.customer,
-                cus_email: order.email,
-                cus_add1: order.address,
+                cus_name: customer,
+                cus_email: email,
+                cus_add1: address,
                 cus_add2: 'Dhaka',
                 cus_city: 'Dhaka',
                 cus_state: 'Dhaka',
                 cus_postcode: '1000',
                 cus_country: 'Bangladesh',
-                cus_phone: order.phone,
+                cus_phone: phone,
                 cus_fax: '01711111111',
                 ship_name: 'Customer Name',
                 ship_add1: 'Dhaka',
                 ship_add2: 'Dhaka',
                 ship_city: 'Dhaka',
                 ship_state: 'Dhaka',
-                ship_postcode: order.postcode,
+                ship_postcode: postcode,
                 ship_country: 'Bangladesh',
             };
-           
+
             const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
             sslcz.init(data).then(apiResponse => {
                 // Redirect the user to payment gateway
-                let GatewayPageURL = apiResponse.GatewayPageURL
-                res.send({url: GatewayPageURL})
-                console.log('Redirecting to: ', GatewayPageURL)
+                let GatewayPageURL = apiResponse.GatewayPageURL;
+                orderCollection.insertOne({
+                    ...order,
+                    transactionId,
+                    paid: false,
+                })
+                res.send({ url: GatewayPageURL });
             });
-
-            res.send(data);
         });
+
+        app.post('/payment/success', async (req, res) => {
+            const { transactionId } = req.query;
+
+            if (!transactionId) {
+                return res.redirect(`${process.env.CLIENT_URL}/payment/fail`)
+            }
+
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    paidAt: new Date()
+                }
+            }
+            const result = await orderCollection.updateOne({ transactionId }, updatedDoc);
+            if (result.modifiedCount > 0) {
+                res.redirect(`${process.env.CLIENT_URL}/payment/success?transactionId=${transactionId}`)
+            }
+        });
+
+        app.post('/payment/fail', async (req, res) => {
+            const { transactionId } = req.query;
+
+            if (!transactionId) {
+                return res.redirect(`${process.env.CLIENT_URL}/payment/fail`);
+            }
+
+            const result = await orderCollection.deleteOne({ transactionId });
+            if (result.deletedCount) {
+                res.redirect(`${process.env.CLIENT_URL}/payment/fail`);
+            }
+        });
+
+        app.get('/order/by-transaction-id/:id', async (req, res) => {
+            const { id } = req.params;
+            const order = await orderCollection.findOne({ transactionId: id });
+            res.send(order);
+        });
+
+
 
         app.get('/orders', async (req, res) => {
             let query = {};
